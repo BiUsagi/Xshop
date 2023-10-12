d65295efbc40b88dcda8032d54e59fb79f315ef3
BiUsagi <tubanovel@gmail.com>
2023-10-13 01:07:12 +0700

    fix 10

--- admin/index.php
+++ admin/index.php
@@ -2,15 +2,6 @@
 include "dao/pdo.php";
 include "includes/connect.php";
 
-
-<<<<<<< HEAD
-//son đã đến đấy
-//cho tao về
-//chán ghê 
-
-
-=======
->>>>>>> 244a770e69700ddef4bc41c21f2d2a410b3ec61e
 ?>
 
 <!-- Header -->
