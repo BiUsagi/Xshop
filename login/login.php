b7e45c277cc29b22d9519b7468ce4e586e924cdb
BiUsagi <tubanovel@gmail.com>
2023-10-14 12:54:38 +0700

    fixx1

--- login/login.php
+++ login/login.php
@@ -1,56 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
+<div class="row mb">
+    <div class="boxtitle">TÀI KHOẢN</div>
+    <div class="boxcontent fomttaikhoan">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <!-- <link rel="stylesheet" href="../css/style.css"> -->
-    <style>
-      
-    </style>
+        <form action="login_index.php?act=login" method="post">
 
-</head>
+            <?php
+            $logout = "index.php?act=logout"; //gan
+            ?>
 
-<body>
-    <div class="row mb">
-        <div class="boxtitle">TÀI KHOẢN</div>
-        <div class="boxcontent fomttaikhoan">
-
-            <form action="login_index.php?act=login" method="post">
-
-                <?php
-                    $logout = "index.php?act=logout"; //gan
-                ?>
-
-                <div class="row mb10">
-                    Tên Đăng Nhập <br>
+            <div class="row mb10">
+                Tên Đăng Nhập <br>
 
 
-                    <input type="text" name="user" id=""><br>
-                </div>
-                <div class="row mb10">
-                    Mật Khẩu
-                    <input type="password" name="pass" id=""> <br>
-                </div>
-                <div class="row mb10">
+                <input type="text" name="user" id=""><br>
+            </div>
+            <div class="row mb10">
+                Mật Khẩu
+                <input type="password" name="pass" id=""> <br>
+            </div>
+            <div class="row mb10">
 
-                    <input type="checkbox" name="" id=""> Ghi nhớ tài khoản?
-                </div>
-                <div class="row mb10">
+                <input type="checkbox" name="" id=""> Ghi nhớ tài khoản?
+            </div>
+            <div class="row mb10">
 
 
-                
+
                 <input type="submit" value="Đăng Nhập" id="">
-                <?php echo '<a href="'.$logout.'">logout</a>' ?>
-                </div>
+                <?php echo '<a href="' . $logout . '">logout</a>' ?>
+            </div>
 
-            </form>
-            <!-- <li><a href="#">Quên mật khẩu</a></li>
+        </form>
+        <!-- <li><a href="#">Quên mật khẩu</a></li>
             <li><a href="<?php $logout ?>">Đăng ký thành viên</a></li> -->
-        </div>
-
     </div>
-</body>
 
-</html>
+</div>
